e6781c405c943f348736a692445755355f75e7d1
Marc Matas <mmatas@student.ndnu.edu>
2020-01-05 23:40:44 -0800

    Initial Commit

---
+++ tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by Tia Perez on 1/5/20.
+//  Copyright © 2020 Matas Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
